be2ad6276761c3c1a2745e68fcabe551483aeb05
Berserk9000 <cruz.d.kevin@gmail.com>
2022-06-30 15:30:23 -0400

    pass length between 8-128

--- Develop/script.js
+++ Develop/script.js
@@ -9,16 +9,27 @@ var uppercaseCheck;
 var numberCheck;
 var specialCheck;
 
+//function for pword length
+function passlength (){
+  passwordLength=prompt("Please choose between 8-128 characters for password length: ");
 
+  if (passwordLength<8){
+    alert("Please choose between 8-128 characters");
+    passlength();
+  }
+  else if (passwordLength>128){
+    alert("Password length must be a number between 8-128 characters");
+    passLength(); 
+  }
+  return passlength
+}
 
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
